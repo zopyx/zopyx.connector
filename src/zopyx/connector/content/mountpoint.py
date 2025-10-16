4cc5b5c272e5fec1817c7dc04a66c4a447ed9202
Andreas Jung <info@zopyx.com>
2025-10-16 07:04:36 +0200

    updated

--- src/zopyx/connector/content/mountpoint.py
+++ src/zopyx/connector/content/mountpoint.py
@@ -8,11 +8,11 @@ from plone.supermodel import model
 
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from zopyx.connector import _
+from zopyx.connector import _
 
 
 class IMountpoint(model.Schema):
@@ -21,8 +21,6 @@ class IMountpoint(model.Schema):
     # If you want, you can load a xml model created TTW here
     # and customize it in Python:
 
-    # model.load('mountpoint.xml')
-
     # directives.widget(level=RadioFieldWidget)
     # level = schema.Choice(
     #     title=_(u'Sponsoring Level'),
@@ -30,15 +28,10 @@ class IMountpoint(model.Schema):
     #     required=True
     # )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    connector_url = schema.URI(
+        title=_(u'Mountpoint URL'),
+        required=False
+    )
 
     # fieldset('Images', fields=['logo', 'advertisement'])
     # logo = namedfile.NamedBlobImage(
