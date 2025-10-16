08eb89fdd085f74ee8a877a1c85c63d8452c1633
Andreas Jung <info@zopyx.com>
2025-10-16 06:31:35 +0200

    updated

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
-        "z3c.jbot",
+        "z3c.jbot==2.2",
         "plone.api>=1.8.4",
         "plone.app.dexterity",
     ],
